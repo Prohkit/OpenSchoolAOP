26f5ef9bea04b59cc4c7629a007f6634d2cfd079
Никита <nikproserg@gmail.com>
2024-07-01 00:05:48 +0800

    initial commit

---
+++ src/main/java/com/example/openschoolaop/OpenSchoolAopApplication.java
@@ -0,0 +1,13 @@
+package com.example.openschoolaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSchoolAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenSchoolAopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/openschoolaop/OpenSchoolAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openschoolaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSchoolAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
