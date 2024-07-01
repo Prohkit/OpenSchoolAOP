a741f04bf8a0ef53d78196d171891cd762e5860d
Никита <nikproserg@gmail.com>
2024-07-02 02:23:17 +0800

    add files for check

---
+++ for_checking/fill_table.sql
@@ -0,0 +1,20 @@
+INSERT INTO method_execution_time_log (id, duration, started_at, finished_at, class_name, method_name)
+VALUES (1, 3804, '2024-07-01 17:42:23.447568', '2024-07-01 17:42:27.251895', 'TestService','methodRandomExecutionTimeAsync');
+INSERT INTO method_execution_time_log (id, duration, started_at, finished_at, class_name, method_name)
+VALUES (2,3674,'2024-07-01 17:42:41.993275','2024-07-01 17:42:45.667281', 'TestService','methodRandomExecutionTimeAsync');
+INSERT INTO method_execution_time_log (id, duration, started_at, finished_at, class_name, method_name)
+VALUES (3,3750,'2024-07-01 17:42:47.395351','2024-07-01 17:42:51.145357', 'TestService','methodRandomExecutionTimeAsync');
+INSERT INTO method_execution_time_log (id, duration, started_at, finished_at, class_name, method_name)
+VALUES (4,2003,'2024-07-01 17:42:51.815271','2024-07-01 17:42:53.818914', 'TestService','methodRandomExecutionTimeAsync');
+INSERT INTO method_execution_time_log (id, duration, started_at, finished_at, class_name, method_name)
+VALUES (5,3758,'2024-07-01 17:42:54.468077','2024-07-01 17:42:58.226289', 'TestService','methodRandomExecutionTimeAsync');
+INSERT INTO method_execution_time_log (id, duration, started_at, finished_at, class_name, method_name)
+VALUES (6,3214,'2024-07-01 17:43:07.332916','2024-07-01 17:43:10.547350', 'TestService','methodRandomExecutionTimeAsync');
+INSERT INTO method_execution_time_log (id, duration, started_at, finished_at, class_name, method_name)
+VALUES (7,1241,'2024-07-01 17:44:03.266858','2024-07-01 17:44:04.508698', 'TestService','methodRandomExecutionTimeAsync');
+INSERT INTO method_execution_time_log (id, duration, started_at, finished_at, class_name, method_name)
+VALUES (8,2522,'2024-07-01 17:44:06.191013','2024-07-01 17:44:08.713514', 'TestService','methodRandomExecutionTimeAsync');
+INSERT INTO method_execution_time_log (id, duration, started_at, finished_at, class_name, method_name)
+VALUES (9,3585,'2024-07-01 17:44:09.340735','2024-07-01 17:44:12.926242', 'TestService','methodRandomExecutionTimeAsync');
+INSERT INTO method_execution_time_log (id, duration, started_at, finished_at, class_name, method_name)
+VALUES (10,3053,'2024-07-01 17:44:13.767688','2024-07-01 17:44:16.820711', 'TestService','methodRandomExecutionTimeAsync');
