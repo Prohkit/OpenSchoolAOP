b838045ff0b35d58e935960a3cfa719053232ad9
Никита <nikproserg@gmail.com>
2024-07-02 02:22:47 +0800

    add new stats

--- src/main/java/com/example/openschoolaop/controller/TimeTrackStatisticController.java
+++ src/main/java/com/example/openschoolaop/controller/TimeTrackStatisticController.java
@@ -1,12 +1,17 @@
 package com.example.openschoolaop.controller;
 
+import com.example.openschoolaop.model.MethodExecutionTimeInfo;
+import com.example.openschoolaop.model.dto.GetExecutionTimeStatsRequest;
+import com.example.openschoolaop.model.dto.MethodExecutionTimeInfoResponse;
 import com.example.openschoolaop.service.TimeTrackStatisticService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -15,15 +20,39 @@ public class TimeTrackStatisticController {
 
     private final TimeTrackStatisticService statisticService;
 
-    @GetMapping(value = "/time/all/{methodName}", produces = "application/json")
-    public ResponseEntity<Long> getAllTimeByMethodName(@PathVariable String methodName) {
-        long value = statisticService.getAllTimeByMethodName(methodName);
+    @GetMapping(value = "/time/class/all", produces = "application/json")
+    public ResponseEntity<Long> getAllTimeByClassNameByInterval(@RequestBody GetExecutionTimeStatsRequest statsRequest) {
+        long value = statisticService.getAllTimeByClassNameByInterval(statsRequest);
         return ResponseEntity.ok(value);
     }
 
-    @GetMapping(value = "/time/average/{methodName}", produces = "application/json")
-    public ResponseEntity<Long> getAverageTimeByMethodName(@PathVariable String methodName) {
-        long value = statisticService.getAverageTimeByMethodName(methodName);
+    @GetMapping(value = "/time/all", produces = "application/json")
+    public ResponseEntity<Long> getAllTimeByClassNameAndMethodNameByInterval(@RequestBody GetExecutionTimeStatsRequest statsRequest) {
+        long value = statisticService.getAllTimeByClassNameAndMethodNameByInterval(statsRequest);
         return ResponseEntity.ok(value);
+    }
+
+    @GetMapping(value = "/time/class/average", produces = "application/json")
+    public ResponseEntity<Long> getAverageTimeByClassNameByInterval(@RequestBody GetExecutionTimeStatsRequest statsRequest) {
+        long value = statisticService.getAverageTimeByClassNameByInterval(statsRequest);
+        return ResponseEntity.ok(value);
+    }
+
+    @GetMapping(value = "/time/average", produces = "application/json")
+    public ResponseEntity<Long> getAverageTimeByClassNameAndMethodNameByInterval(@RequestBody GetExecutionTimeStatsRequest statsRequest) {
+        long value = statisticService.getAverageTimeByClassNameAndMethodNameByInterval(statsRequest);
+        return ResponseEntity.ok(value);
+    }
+
+    @GetMapping(value = "/info", produces = "application/json")
+    public ResponseEntity<MethodExecutionTimeInfoResponse> getMethodExecutionTimeInfoByInterval(@RequestBody GetExecutionTimeStatsRequest statsRequest) {
+        List<MethodExecutionTimeInfo> timeInfos = statisticService
+                .getMethodExecutionTimeInfoByInterval(
+                        statsRequest.getClassName(),
+                        statsRequest.getMethodName(),
+                        statsRequest.getFrom(),
+                        statsRequest.getTo());
+        MethodExecutionTimeInfoResponse timeInfoResponse = new MethodExecutionTimeInfoResponse(timeInfos);
+        return ResponseEntity.ok(timeInfoResponse);
     }
 }
--- src/main/java/com/example/openschoolaop/service/TimeTrackStatisticService.java
+++ src/main/java/com/example/openschoolaop/service/TimeTrackStatisticService.java
@@ -1,12 +1,17 @@
 package com.example.openschoolaop.service;
 
 import com.example.openschoolaop.model.MethodExecutionTimeInfo;
+import com.example.openschoolaop.model.dto.GetExecutionTimeStatsRequest;
 import com.example.openschoolaop.repository.MethodExecutionTimeInfoRepository;
+import jakarta.persistence.criteria.Predicate;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.OptionalDouble;
 
 @Service
@@ -16,17 +21,65 @@ public class TimeTrackStatisticService {
 
     private final MethodExecutionTimeInfoRepository timeInfoRepository;
 
-    public Long getAllTimeByMethodName(String methodName) {
-        return timeInfoRepository.getMethodExecutionTimeInfosByMethodName(methodName)
+    public Long getAllTimeByClassNameByInterval(GetExecutionTimeStatsRequest timeStatsRequest) {
+        return getMethodExecutionTimeInfoByInterval(timeStatsRequest.getClassName(), null,
+                timeStatsRequest.getFrom(), timeStatsRequest.getTo())
                 .stream().mapToLong(MethodExecutionTimeInfo::getDuration).sum();
     }
 
-    public Long getAverageTimeByMethodName(String methodName) {
-        OptionalDouble optionalDouble = timeInfoRepository.getMethodExecutionTimeInfosByMethodName(methodName)
+    public Long getAllTimeByClassNameAndMethodNameByInterval(GetExecutionTimeStatsRequest timeStatsRequest) {
+        return getMethodExecutionTimeInfoByInterval(timeStatsRequest.getClassName(),
+                timeStatsRequest.getMethodName(), timeStatsRequest.getFrom(), timeStatsRequest.getTo())
+                .stream().mapToLong(MethodExecutionTimeInfo::getDuration).sum();
+    }
+
+    public Long getAverageTimeByClassNameByInterval(GetExecutionTimeStatsRequest timeStatsRequest) {
+        OptionalDouble optionalDouble = getMethodExecutionTimeInfoByInterval(timeStatsRequest.getClassName(),
+                null, timeStatsRequest.getFrom(), timeStatsRequest.getTo())
                 .stream().mapToLong(MethodExecutionTimeInfo::getDuration).average();
         if (optionalDouble.isPresent()) {
             return (long) optionalDouble.getAsDouble();
         }
         return 0L;
+    }
+
+    public Long getAverageTimeByClassNameAndMethodNameByInterval(GetExecutionTimeStatsRequest timeStatsRequest) {
+        OptionalDouble optionalDouble = getMethodExecutionTimeInfoByInterval(timeStatsRequest.getClassName(),
+                timeStatsRequest.getMethodName(), timeStatsRequest.getFrom(), timeStatsRequest.getTo())
+                .stream().mapToLong(MethodExecutionTimeInfo::getDuration).average();
+        if (optionalDouble.isPresent()) {
+            return (long) optionalDouble.getAsDouble();
+        }
+        return 0L;
+    }
+
+    public List<MethodExecutionTimeInfo> getMethodExecutionTimeInfoByInterval
+            (String className,
+             String methodName,
+             LocalDateTime from,
+             LocalDateTime to) {
+        if (className == null && methodName == null && from == null && to == null) {
+            return new ArrayList<>();
+        }
+        return timeInfoRepository.findAll((root, query, cb) -> {
+            Predicate conjunction = cb.conjunction();
+            if (Objects.nonNull(className)) {
+                Predicate like = cb.like(cb.upper(root.get("className")), "%" + className.toUpperCase() + "%");
+                conjunction = cb.and(conjunction, like);
+            }
+            if (Objects.nonNull(methodName)) {
+                Predicate like = cb.like(cb.upper(root.get("methodName")), "%" + methodName.toUpperCase() + "%");
+                conjunction = cb.and(conjunction, like);
+            }
+            if (Objects.nonNull(from)) {
+                Predicate greaterThanOrEqualTo = cb.greaterThanOrEqualTo(root.get("startedAt"), cb.literal(from));
+                conjunction = cb.and(conjunction, greaterThanOrEqualTo);
+            }
+            if (Objects.nonNull(to)) {
+                Predicate lessThanOrEqualTo = cb.lessThanOrEqualTo(root.get("finishedAt"), cb.literal(to));
+                conjunction = cb.and(conjunction, lessThanOrEqualTo);
+            }
+            return conjunction;
+        });
     }
 }
