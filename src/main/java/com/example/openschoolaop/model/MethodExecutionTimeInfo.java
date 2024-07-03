2631d9add7208fb25e2661a58fa6947c4a04c19c
Никита <nikproserg@gmail.com>
2024-07-03 18:49:18 +0800

    refactor model

--- src/main/java/com/example/openschoolaop/model/MethodExecutionTimeInfo.java
+++ src/main/java/com/example/openschoolaop/model/MethodExecutionTimeInfo.java
@@ -26,7 +26,7 @@ public class MethodExecutionTimeInfo {
     }
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "class_name")
--- src/main/java/com/example/openschoolaop/model/dto/GetExecutionTimeStatsRequest.java
+++ src/main/java/com/example/openschoolaop/model/dto/GetExecutionTimeStatsRequest.java
@@ -1,23 +1,19 @@
 package com.example.openschoolaop.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class GetExecutionTimeStatsRequest {
 
-    @JsonProperty("class_name")
     private String className;
 
-    @JsonProperty("method_name")
     private String methodName;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime from;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime to;
 }
